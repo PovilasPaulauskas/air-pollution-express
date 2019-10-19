49188e7fe1b70c1210c01e1dd2e24066f21aef25
vercinskij <verc@danskebank.lt>
2019-10-19 15:27:02 +0300

    Added csv to json parsing library

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express()
 const port = 3000
 
 const files = [
-    './data/US_LosAngeles-LongBeach-SantaAna_Anaheim.MOD04',
-    './data/US_LosAngeles-LongBeach-SantaAna_Glendora-Laurel.MOD04',
-    './data/US_LosAngeles-LongBeach-SantaAna_LosAngeles-N.Mai.MOD04',
-    './data/US_LosAngeles-LongBeach-SantaAna_Reseda.MOD04',
-    './data/US_LosAngeles-LongBeach-SantaAna_SantaClarita.MOD04',
-    './data/US_LosAngeles-LongBeach-SantaAna_SouthLongBeach.MOD04'];
+    './data/US_LosAngeles-LongBeach-SantaAna_Anaheim.MOD04.csv',
+    './data/US_LosAngeles-LongBeach-SantaAna_Glendora-Laurel.MOD04.csv',
+    './data/US_LosAngeles-LongBeach-SantaAna_LosAngeles-N.Mai.MOD04.csv',
+    './data/US_LosAngeles-LongBeach-SantaAna_Reseda.MOD04.csv',
+    './data/US_LosAngeles-LongBeach-SantaAna_SantaClarita.MOD04.csv',
+    './data/US_LosAngeles-LongBeach-SantaAna_SouthLongBeach.MOD04.csv'];
+
+
+async function loadData(path) {
+    return await csv({
+        noheader: true, 
+        headers: ['year','month','day','latitude','longitude','aod1','aod3','std3']
+    }).fromFile(path);
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
